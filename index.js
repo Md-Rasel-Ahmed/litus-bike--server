28a5e91df341696628ee1344f4bb626b972e1ec7
Rasel-ahemd <nhd305812@gmail.com>
2022-04-29 00:22:59 +0500

    database cerate

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,4 +12,28 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Api done");
 });
+
+const uri =
+  "mongodb+srv://mongodbUser:hSMFLPo6zrAqzr8w@cluster0.6plls.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("storedBike").collection("product");
+    let obj = { name: "rasel", age: 21 };
+    const result = await productCollection.insertOne(obj);
+    app.get("/product", (req, res) => {
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port);
