3ff2a38c9e7e839a288eafb561415e54b402be8f
Rasel-ahemd <nhd305812@gmail.com>
2022-04-30 01:00:40 +0500

    quantity update success

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -25,10 +26,40 @@ async function run() {
   try {
     await client.connect();
     const productCollection = client.db("storedBike").collection("product");
-    let obj = { name: "rasel", age: 21 };
-    const result = await productCollection.insertOne(obj);
-    app.get("/product", (req, res) => {
+    // let obj = { name: "rasel", age: 21 };
+    // const result = await productCollection.insertOne(obj);
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
       res.send(result);
+    });
+    // find product
+    app.get("/product/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Update quantity
+    app.put("/product/:id", async (req, res) => {
+      const { id } = req.params;
+      const updateQuantity = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity: updateQuantity.quantity,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+      console.log(result);
     });
   } finally {
     // await client.close();
