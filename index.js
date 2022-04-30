4065be3f112459599ab4da79d70ac55a4a487eac
Rasel-ahemd <nhd305812@gmail.com>
2022-04-30 23:25:08 +0500

    .env requir file added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
